e981cf3e272b41b477f85a45a3cfd52ff0c24b27
OrionTRC <74710197+OrionTRC@users.noreply.github.com>
2024-01-07 22:34:26 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+{
+    const opener = document.getElementById("menuOpen");
+    document.addEventListener("keydown", (event) => {
+      if (event.key === "Escape") opener.blur();     
+    } );
+    
+    opener.addEventListener("touchstart", () => {
+      opener.focus();
+    } );
+    opener.nextElementSibling.addEventListener("touchstart",  () => {
+      opener.blur();
+    } );
+}  
+
+const pipe=(x,...fns)=>fns.reduce((v,f)=>f(v),x);
+const flow =(...fns)=>fns.reduce((v,f)=>f(v));
+const map =(functor)=>(arr)=>arr.map(functor);
