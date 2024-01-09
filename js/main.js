4c94170c0aeef8ebe5a84c1da69b5fcc3b51d322
OrionTRC <74710197+OrionTRC@users.noreply.github.com>
2024-01-09 19:41:22 +0100

    touchstart fix fails

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
-{
-    const opener = document.getElementById('menuOpen');
-    document.addEventListener("keydown", (event) => {
-      if (event.key === "Escape") opener.blur();     
-    } );
+// {
+//     const opener = document.getElementById('menuOpen');
+//     document.addEventListener("keydown", (event) => {
+//       if (event.key === "Escape") opener.blur();     
+//     } );
     
-    opener.addEventListener("touchstart", () => {
-      opener.focus();
-    } );
-    opener.nextElementSibling.addEventListener("touchstart",  () => {
-      opener.blur();
-    } );
-}  
+//     opener.addEventListener("touchstart", () => {
+//       opener.focus();
+//     } );
+//     opener.nextElementSibling.addEventListener("touchstart",  () => {
+//       opener.blur();
+//     } );
+// }  
+//Opener fix doesn't work on phone??
 
 const pipe=(x,...fns)=>fns.reduce((v,f)=>f(v),x);
 const flow =(...fns)=>fns.reduce((v,f)=>f(v));
