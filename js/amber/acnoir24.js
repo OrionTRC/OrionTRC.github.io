fff2d4accfac338a68af8dc7183550d3a7ba528e
OrionTRC <74710197+OrionTRC@users.noreply.github.com>
2024-01-13 08:08:33 +0100

    import attempt
    trying to import jen

---
+++ js/amber/acnoir24.js
@@ -0,0 +1,53 @@
+{
+  const main=document.querySelector('main');
+  import('./jen.js').then((mod)=>{
+    mod.loadPageInto(main);
+  });
+}
+
+function Acnoir24() {
+  var self = this;
+
+  self.games = ko.observableArray([
+    {
+        name:'The Tall Man'
+        , title:'Burning Coal'
+        , slot:'5-2'
+    }
+    , {
+      name: 'Carloman Randolph'
+      , title: 'Who killed Julian'
+      , slot:'6-3'
+    }
+    , {
+      name:''
+      , title:'A brand new world'
+      , slot:'7-2'
+    }
+    , {
+      name:''
+      , title:'Excuse me, are we related?'
+      , slot:'8-1'
+    }
+    , jen()
+    , {
+      name:''
+      , title:'Aurellis (Edwardiana)'
+      , slot:'10-3'
+    }
+    , {
+      name:''
+      , title:'My first heist'
+      , slot:'115'
+    }
+  ]);
+
+  self.selectedGame = ko.observable({});
+
+  self.selectGame = function(game,event){
+    self.selectedGame(game);
+    event.target.closest('button').blur();
+  };
+};
+
+ko.applyBindings(new Acnoir24());
---
+++ js/amber/jen.js
@@ -0,0 +1,46 @@
+function jen() {
+    return {
+        name: 'Jennifer Rennes'
+        , title: 'Long Live the Queen'
+        , slot: '9-5'
+        , stats: [
+            { stat: 'Psyche', value: 20, rank:'Amber' }
+            , { stat: 'Strength', value: 0, rank:'Amber'  }
+            , { stat: 'Endurance', value: 5, rank:'Amber'  }
+            , { stat: 'Warfare', value: 35, rank:'Amber'  }
+        ]
+        , powers: [
+            Object.assign(pattern, { cost: 0, details: 'free from character creation' })
+            , conjuration
+            , Object.assign(powerWords, {
+                type: 'Schang, Krolak, Askiir, Jask, Marsk'
+                , details: 'True Form, Neural Disrupt, Psyche Disrupt, Trump Disrupt, Weaken Structure'
+            })
+            , { name: 'Jewel of Rebma', type: 'initiate', cost: 10, details: 'Burnt into her left hand' }
+        ]
+        , items: [
+            {
+                name: 'Studded glove'
+                , cost: 6
+                , qualities: 'deadly damage,alternate shapes'
+                , quantity: 1
+                , description: 'Glove, Gauntlet, Cestus, Dagger, Rapier, Claymore, Scissors, Pliers, Bolt cutter,  Crowbar, Baseball bat, Necklace'
+            }
+            , {
+                name: 'Suitable armor'
+                , cost: 10
+                , qualities: 'Impervious,Limited Shape Shift,Danger Sense'
+                , quantity: 1
+                , description: 'This item takes any shape or form its wearer psychically calls for. It shifts into battle armor form automatically when it senses danger.'
+            }
+            , {
+                name: 'Deck of tricks'
+                , cost: 3
+                , qualities: 'Mold shadow stuff'
+                , quantity: 52
+                , description: 'Whisper the desired effect and deal the card.'
+            }
+        ]
+        , stuff: { type: 'Good', value: 1 }
+    };
+};
