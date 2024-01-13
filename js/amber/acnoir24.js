5aaa541bcc3470be462deaada793c888425ccaf2
OrionTRC <74710197+OrionTRC@users.noreply.github.com>
2024-01-13 08:16:09 +0100

    Add files via upload

---
+++ js/amber/acnoir24.js
@@ -0,0 +1,52 @@
+{ 
+  import('./jen.js').then((mod)=>{
+    mod.load();
+  });
+}
+
+function Acnoir24() {
+  var self = this;
+
+  self.games = ko.observableArray([
+    {
+        name:'The Tall Man'
+        , title:'Burning Coal'
+        , slot:'5-2'
+    }
+    , {
+      name: 'Carloman Randolph'
+      , title: 'Who killed Julian'
+      , slot:'6-3'
+    }
+    , {
+      name:''
+      , title:'A brand new world'
+      , slot:'7-2'
+    }
+    , {
+      name:''
+      , title:'Excuse me, are we related?'
+      , slot:'8-1'
+    }
+    , jen()
+    , {
+      name:''
+      , title:'Aurellis (Edwardiana)'
+      , slot:'10-3'
+    }
+    , {
+      name:''
+      , title:'My first heist'
+      , slot:'115'
+    }
+  ]);
+
+  self.selectedGame = ko.observable({});
+
+  self.selectGame = function(game,event){
+    self.selectedGame(game);
+    event.target.closest('button').blur();
+  };
+};
+
+ko.applyBindings(new Acnoir24());
---
+++ js/amber/powers.js
@@ -0,0 +1,19 @@
+const pattern={
+    name:'Pattern'
+    , type:'imprint'
+    , cost:50
+    , details:''
+}
+const conjuration={
+    name:'Conjuration'
+    , type:'basic'
+    , cost:20
+    , details:''
+}
+const powerWords={
+    name:'Power words'
+    , type:''
+    , cost:10
+    , details:''
+}
+
