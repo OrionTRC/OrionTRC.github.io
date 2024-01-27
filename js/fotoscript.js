3b907d683c12bacc7a3ddb5f7eefcd6de6fe3b1c
OrionTRC <74710197+OrionTRC@users.noreply.github.com>
2024-01-27 16:14:16 +0100

    Add files via upload

---
+++ js/fotoscript.js
@@ -0,0 +1,11 @@
+function fotomodel(){
+    var self=this;
+
+    self.photoArray=ko.observableArray();
+
+    for(let i=1;i<11;i++){
+        self.photoArray.push({photo:`assets\photos\Huwelijkskaarten${i}.zip`,title:`Huwelijkskaarten ${i}`});
+    }
+};
+
+ko.applyBindings(new fotomodel());
