f79100ca12479997ee4d37d3ad35e6f67344e791
OrionTRC <74710197+OrionTRC@users.noreply.github.com>
2024-01-28 18:42:01 +0100

    Add files via upload

---
+++ js/amber/acnoir.js
@@ -0,0 +1,40 @@
+import {tallman} from './acn24.js';
+import {carl} from './acn24.js';
+import {jen} from './acn24.js';
+import {reno} from './acn24.js';
+
+
+function acnoir() {
+  var self = this;
+
+  self.games = ko.observableArray([
+    tallman?.()
+    , carl?.()
+    , {
+      name:''
+      , title:'A brand new world'
+      , slot:'7-2'
+    }
+    , reno?.()
+    , jen?.()
+    , {
+      name:''
+      , title:'Aurellis (Edwardiana)'
+      , slot:'10-3'
+    }
+    , {
+      name:'Zach Whitfield'
+      , title:'My first heist'
+      , slot:'11-5'
+    }
+  ]);
+
+  self.selectedGame = ko.observable({});
+
+  self.selectGame = function(game,event){
+    self.selectedGame(game);
+    event.target.closest('button').blur();
+  };
+};
+
+ko.applyBindings(new acnoir());
--- js/fotoscript.js
+++ js/fotoscript.js
@@ -1,10 +1,20 @@
 function fotomodel(){
     var self=this;
 
-    self.photoArray=ko.observableArray();
-
+    self.zipArray=ko.observableArray();
     for(let i=1;i<11;i++){
-        self.photoArray.push({photo:`assets/photos/Huwelijkskaarten${i}.zip`,title:`Huwelijkskaarten ${i}`});
+        self.photoArray.push({photo:`assets/zip/Huwelijkskaarten${i}.zip`,title:`Huwelijkskaarten ${i}`});
+    }
+
+    self.photoArray=ko.observableArray();
+    
+    let fs = require('fs');
+    let photos= fs.readdirSync('../assets/photos/');
+    for(let i=0,photo;photo=photos[i];i++){
+        self.photoArray.push({
+            link:`../assets/photos/${photo}`
+            , source: require(`../assets/photos${photo}`)
+        });
     }
 };
 
