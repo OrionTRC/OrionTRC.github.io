52d235cb02736ee393c43db6fe518efc4c0be4b1
OrionTRC <74710197+OrionTRC@users.noreply.github.com>
2024-12-18 23:29:29 +0100

    Update acnoir.js

--- js/amber/acnoir.js
+++ js/amber/acnoir.js
@@ -7,10 +7,10 @@ function acnoir() {
 
     self.selectedGame = ko.observable({});
 
-    self.importYear = function (e, el) {
+    self.importYear = function (e) {
         e.preventDefault();
         const amberBody = document.getElementById('bodyCover');
-        const yr = el.dataset.year;
+        const yr = e.target.dataset.year;
 
         import(`./acn_${yr}.js`).then((mod) => {
             yearBinding = mod;
