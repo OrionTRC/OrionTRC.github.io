7beb360b2bfaba9362e7bd1a7da70201fd970e5e
OrionTRC <74710197+OrionTRC@users.noreply.github.com>
2024-12-18 15:29:00 +0100

    Add files via upload

--- js/fotoscript.js
+++ js/fotoscript.js
@@ -1,31 +1,37 @@
-function fotomodel(photolist){
-    var self=this;
+function fotomodel(photolist) {
+    var self = this;
 
-    self.zipArray=ko.observableArray();
-    for(let i=1;i<11;i++){
-        self.zipArray.push({photo:`assets/zip/Huwelijkskaarten${i}.zip`,title:`Huwelijkskaarten ${i}`});
+    self.zipArray = ko.observableArray();
+    for (let i = 1; i < 11; i++) {
+        self.zipArray.push({ photo: `assets/zip/Huwelijkskaarten${i}.zip`, title: `Huwelijkskaarten ${i}` });
     }
 
-    const lostNumbers = [4, 6, 8, 11, 22, 31, 36, 37];        
-    if(photolist=document.getElementById('image-list')){
-         for(let i=1,li,photo,href,dataUrl;i<46;i++){
-            if(!lostNumbers.includes(i)){
-                href = `assets/photos/${i}.jpg`;
-                dataUrl = `assets/thumbs/tmb_${i}.jpg`;
+    const lostNumbers = [4, 6, 8, 11, 22, 31, 36, 37];
+    if (photolist = document.getElementById('image-list')) {       
+        for (let i = 1; i < 46; i++) {
+            if (!lostNumbers.includes(i)) {               
+                makeThumbnail(i);
+            }
+        }
+    }
 
-                li=add(photolist,'li');
-                photo=add(li,'a',{
+    function makeThumbnail(i) {
+        return new Promise((resolve, reject) => {
+            const
+                href = `assets/photos/${i}.jpg`
+                , dataUrl = `assets/thumbs/tmb_${i}.jpg`
+                , li = add(photolist, 'li')
+                , photo = add(li, 'a', {
                     href
-                    , target:'_blank'
+                    , target: '_blank'
                     , download: `${i}`
                 });
-                photo.style.backgroundImage=`url(./${dataUrl})`;
-            }
-            
-            //self.photoArray.push(`assets/photos/${i}.jpg`);
 
-        }
-    }   
+            photo.style.backgroundImage = `url(./${dataUrl})`;
+            resolve(li);
+        });
+    }
 };
+
 
 ko.applyBindings(new fotomodel());
