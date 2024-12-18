3c35106406c601d634d8d352803071c8039a0cc9
OrionTRC <74710197+OrionTRC@users.noreply.github.com>
2024-12-18 22:25:44 +0100

    Add files via upload

--- js/amber/acnoir.js
+++ js/amber/acnoir.js
@@ -1,22 +1,28 @@
-function importYear(e, el) {
-    e.preventDefault();
-    const amberBody = document.getElementById('bodyCover');
-    const yr = el.dataset.year;
-    
-    import(`./acn_${yr}.js`).then((mod) => {
-        ko.applyBindings(new acnoir(mod.acn()));
-    });  
-};
 
-function acnoir(ac) {
+function acnoir() {
     var self = this;
-    const gameset = [...Object.values(ac)];   
-    self.games = ko.observableArray(gameset);
+
+    sel.gameset = [];
+    self.games = ko.observableArray([]);
 
     self.selectedGame = ko.observable({});
+
+    self.importYear = function (e, el) {
+        e.preventDefault();
+        const amberBody = document.getElementById('bodyCover');
+        const yr = el.dataset.year;
+
+        import(`./acn_${yr}.js`).then((mod) => {
+            yearBinding = mod;
+            gameset = [...Object.values(mod.acn())];
+            self.games = ko.observableArray(gameset);
+        });
+    };
 
     self.selectGame = function (game, event) {
         self.selectedGame(game);
         event.target.closest('button').blur();
     };
 };
+
+ko.applyBindings(new acnoir());
